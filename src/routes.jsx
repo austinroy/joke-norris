b7d5420e77095ba42027e8a8b123498953436222
Austin Roy <austin.roy@andela.com>
2018-09-06 13:02:11 +0300

    Fix linting errors

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { 
-    Route,
-    Switch 
-} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import App from './App';
 
 import Categories from './components/Categories';
@@ -10,13 +7,13 @@ import NotFound from './components/NotFound';
 import SingleJoke from './components/SingleJoke';
 
 const Routes = () => (
-    <App>
-        <Switch >
-            <Route exact path="/" component={Categories} />
-            <Route exact path="/categories/:category" component={SingleJoke} />
-            <Route component={NotFound} />
-        </Switch>
-    </App>
-)
+  <App>
+    <Switch>
+      <Route exact path="/" component={Categories} />
+      <Route exact path="/categories/:category" component={SingleJoke} />
+      <Route component={NotFound} />
+    </Switch>
+  </App>
+);
 
 export default Routes;
